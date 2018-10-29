4363e815da3ded706cefdd6b9e85682772075467
User <ezzy_nauzzee@hotmail.com>
2018-10-29 17:38:25 +0800

    Updated again!

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < Clearance::UsersController
+
+  private
+
+  def user_from_params
+    email = user_params.delete(:email)
+    password = user_params.delete(:password)
+    first_name = user_params.delete(:first_name)
+    last_name = user_params.delete(:last_name)
+    phone_number = user_params.delete(:phone_number)
+
+    Clearance.configuration.user_model.new(user_params).tap do |user|
+      user.email = email
+      user.password = password
+      user.first_name = first_name
+      user.last_name = last_name
+      user.phone_number = phone_number
+    end
+  end
+
+end
