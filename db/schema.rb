c2a71f4fca25f88f5941cad6f3af40ead41fcb54
User <ezzy_nauzzee@hotmail.com>
2018-10-31 00:32:26 +0800

    Added more appropriate columns to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_162000) do
+ActiveRecord::Schema.define(version: 2018_31_10_002000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2018_10_30_162000) do
     t.datetime "updated_at", null: false
     t.string "price_per_day"
     t.bigint "user_id"
+    t.string "description"
+    t.string "amenities"
+    t.integer "guests"
+    t.string "bedrooms"
+    t.string "beds"
+    t.string "sleeping_arrangements"
+    t.string "baths"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
